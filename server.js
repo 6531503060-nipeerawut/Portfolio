17a584173044aea6b7663b1474d61a9f461d41b5
AUYOF-eng <122779705+6531503060-nipeerawut@users.noreply.github.com>
2026-03-10 14:50:37 +0700

    updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`)
-);
+module.exports = app;
